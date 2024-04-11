5afa45daff408a649a5bfd8cf8cfbabdc0a4dc48
mitalijambhulkar67 <123642577+mitalijambhulkar67@users.noreply.github.com>
2024-04-11 12:26:03 +0530

    Create App.java

---
+++ Vending_Machine/src/App.java
@@ -0,0 +1,70 @@
+import coins.Coin;
+import item.Item;
+import item.ItemShelf;
+import item.ItemType;
+import vendingmachine.VendingMachine;
+import vendingmachine.VendingMachineState;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        VendingMachine vendingMachine = new VendingMachine();
+
+        System.out.println("Fill the inventory");
+        fillInventory(vendingMachine);
+
+        System.out.println("Display the Inventory");
+        displayInventory(vendingMachine);
+
+        VendingMachineState vendingMachineState = vendingMachine.getVendingMachineState();
+
+        vendingMachineState.insertCoin(vendingMachine, Coin.TWENTY);
+        vendingMachineState.insertCoin(vendingMachine, Coin.FIFTY);
+
+        vendingMachineState.chooseProduct(vendingMachine, 102);
+
+        displayInventory(vendingMachine);
+    }
+
+    private static void fillInventory(VendingMachine vendingMachine) {
+        ItemShelf[] itemShelfSlots = vendingMachine.getInventory().getInventory();
+        for (int i = 0; i < itemShelfSlots.length; i++) {
+            Item item = new Item();
+            if (i == 0 || i == 8) {
+                item.setItemType(ItemType.OREO);
+                item.setPrice(30);
+            } else if (i == 1 || i == 9) {
+                item.setItemType(ItemType.JUICE);
+                item.setPrice(40);
+            } else if (i == 2) {
+                item.setItemType(ItemType.COKE);
+                item.setPrice(45);
+            } else if (i == 3) {
+                item.setItemType(ItemType.SODA);
+                item.setPrice(20);
+            } else if (i == 4) {
+                item.setItemType(ItemType.CHIPS);
+                item.setPrice(20);
+            } else if (i == 5) {
+                item.setItemType(ItemType.CHOCOLATE);
+                item.setPrice(60);
+            } else if (i == 6) {
+                item.setItemType(ItemType.CANDY);
+                item.setPrice(5);
+            } else if (i == 7) {
+                item.setItemType(ItemType.GUM);
+                item.setPrice(5);
+            }
+            itemShelfSlots[i].setItem(item);
+            itemShelfSlots[i].setSoldOut(false);
+        }
+    }
+
+    private static void displayInventory(VendingMachine vendingMachine) {
+        ItemShelf[] itemShelfSlots = vendingMachine.getInventory().getInventory();
+        for (ItemShelf shelf : itemShelfSlots) {
+            System.out.println("Item Code: " + shelf.getItemCode() + " Item: " + shelf.getItem().getType() + " Price: "
+                    + shelf.getItem().getPrice() +
+                    " isAvailable:" + !shelf.isSoldOut());
+        }
+    }
+}
