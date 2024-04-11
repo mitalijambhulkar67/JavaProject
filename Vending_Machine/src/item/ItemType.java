9001809389bb559edbd900959ca3ce62951b7c64
mitalijambhulkar67 <123642577+mitalijambhulkar67@users.noreply.github.com>
2024-04-11 12:22:29 +0530

    Create ItemType.java

---
+++ Vending_Machine/src/item/ItemType.java
@@ -0,0 +1,22 @@
+package item;
+
+public enum ItemType {
+    OREO("OREO"), // i = 0,8
+    JUICE("JUICE"), // i =1,9
+    COKE("COKE"), // i =2
+    SODA("SODA"), // i = 3
+    CHIPS("CHIPS"), // i = 4
+    CHOCOLATE("CHOCOLATE"), // i =5
+    CANDY("CANDY"), // i =6
+    GUM("GUM"); // i=7
+
+    private final String type;
+
+    ItemType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+}
