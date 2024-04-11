976488516bf8331fb68f21a3cbbf0436282137e1
mitalijambhulkar67 <123642577+mitalijambhulkar67@users.noreply.github.com>
2024-04-11 12:23:56 +0530

    Create VendingMachine.java

---
+++ Vending_Machine/src/vendingmachine/VendingMachine.java
@@ -0,0 +1,43 @@
+package vendingmachine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import coins.Coin;
+import inventory.Inventory;
+
+public class VendingMachine {
+    private VendingMachineState vendingMachineState;
+    private Inventory inventory;
+    private List<Coin> coinList;
+
+    public VendingMachine() {
+        vendingMachineState = new VendingMachineState();
+        inventory = new Inventory(10);
+        coinList = new ArrayList<>();
+    }
+
+    public VendingMachineState getVendingMachineState() {
+        return this.vendingMachineState;
+    }
+
+    public void setVendingMachineState(VendingMachineState vendingMachineState) {
+        this.vendingMachineState = vendingMachineState;
+    }
+
+    public Inventory getInventory() {
+        return this.inventory;
+    }
+
+    public void setInventory(Inventory inventory) {
+        this.inventory = inventory;
+    }
+
+    public List<Coin> getCoinList() {
+        return this.coinList;
+    }
+
+    public void setCoinList(List<Coin> coinList) {
+        this.coinList = coinList;
+    }
+}
