e7f6dac2082fa6319bfa073fa73b2151b2da8f9c
mitalijambhulkar67 <123642577+mitalijambhulkar67@users.noreply.github.com>
2024-04-11 12:11:56 +0530

    Create Coin.java

---
+++ Vending_Machine/src/coins/Coin.java
@@ -0,0 +1,21 @@
+package coins;
+
+public enum Coin {
+    ONE(1),
+    TWO(2),
+    FIVE(3),
+    TEN(10),
+    TWENTY(20),
+    FIFTY(50),
+    HUNDRED(100);
+
+    private final int value;
+
+    Coin(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+}
