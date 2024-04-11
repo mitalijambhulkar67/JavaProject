64182c233db8cba45f588c3683eb99b01d1498d1
mitalijambhulkar67 <123642577+mitalijambhulkar67@users.noreply.github.com>
2024-04-11 12:21:52 +0530

    Create Item.java

---
+++ Vending_Machine/src/item/Item.java
@@ -0,0 +1,22 @@
+package item;
+
+public class Item {
+    private ItemType itemType;
+    private int price;
+
+    public ItemType getType() {
+        return this.itemType;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public void setItemType(ItemType itemType) {
+        this.itemType = itemType;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
