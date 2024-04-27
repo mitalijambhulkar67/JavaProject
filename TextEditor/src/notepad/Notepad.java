9e0d849d0411e41c967b3079ded94bf376d17697
mitalijambhulkar67 <123642577+mitalijambhulkar67@users.noreply.github.com>
2024-04-27 12:21:58 +0530

    Create Notepad.java

---
+++ TextEditor/src/notepad/Notepad.java
@@ -0,0 +1,136 @@
+package notepad;
+
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+import action.Action;
+
+public class Notepad {
+    private int id = 1;
+    private String[] notepad;
+    private Stack<Action> undoAction;
+    private Stack<Action> redoAction;
+    private Queue<String> clipBoard;
+    private int capacity;
+
+    public Notepad(int capacity) {
+        notepad = new String[capacity];
+
+        for (int i = 0; i < this.capacity; i++) {
+            notepad[i] = "";
+        }
+
+        undoAction = new Stack<>();
+        redoAction = new Stack<>();
+        clipBoard = new LinkedList<>();
+        this.capacity = capacity;
+    }
+
+    public void display() {
+        for (int i = 0; i < this.capacity; i++) {
+            if (notepad[i] != null) {
+                System.out.println(notepad[i]);
+            } else
+                System.out.println();
+        }
+    }
+
+    public void display(int startingLine, int endingLine) {
+        if (startingLine <= 0 || startingLine > endingLine || endingLine > this.capacity) {
+            System.out.println("Unable to delete, please check the inputs");
+            return;
+        }
+
+        for (int i = startingLine - 1; i < endingLine; i++) {
+            if (notepad[i] != null) {
+                System.out.println(notepad[i]);
+            }
+        }
+    }
+
+    public void insertLine(int lineNumber, String text) {
+        if (lineNumber > capacity) {
+            System.out.println("Notepad limit crossed...");
+            return;
+        }
+
+        notepad[lineNumber - 1] = text;
+        undoAction.push(new Action(id++, LocalDateTime.now(), true, lineNumber, text));
+        display();
+    }
+
+    public void delete(int lineNumber) {
+        if (lineNumber > capacity) {
+            System.out.println("No such row to delete");
+        }
+
+        if (notepad[lineNumber - 1] == null) {
+            System.out.println("Nothing to delete");
+        }
+
+        redoAction.push(new Action(id++, LocalDateTime.now(), false, lineNumber, notepad[lineNumber - 1]));
+        notepad[lineNumber - 1] = "";
+        display();
+    }
+
+    public void delete(int startingLine, int endingLine) {
+        if (startingLine > endingLine || endingLine > capacity) {
+            System.out.println("Unable to delete, please check the inputs");
+            return;
+        }
+
+        for (int i = startingLine; i <= endingLine; i++) {
+            delete(i);
+        }
+    }
+
+    public void copy(int startingLine, int endingLine) {
+        if (startingLine > endingLine || endingLine > this.capacity) {
+            System.out.println("Unable to copy");
+        }
+
+        String copyText = "";
+        for (int i = startingLine; i <= endingLine; i++) {
+            copyText += (notepad[i] + "\n");
+        }
+
+        if (!copyText.isEmpty())
+            clipBoard.add(copyText);
+    }
+
+    public void paste(int lineNumber) {
+        if (this.clipBoard.isEmpty()) {
+            System.out.println("Nothing to paste");
+            return;
+        }
+
+        String text = clipBoard.peek();
+        insertLine(lineNumber, text);
+    }
+
+    public void undo() {
+        if (this.undoAction.isEmpty()) {
+            System.out.println("Nothing to undo");
+            return;
+        }
+
+        Action action = this.undoAction.peek();
+        this.undoAction.pop();
+
+        delete(action.getLineNumber());
+    }
+
+    public void redo() {
+        if (this.redoAction.isEmpty()) {
+            System.out.println("Nothing to redo");
+            return;
+        }
+
+        Action action = this.redoAction.peek();
+        this.redoAction.pop();
+
+        insertLine(action.getLineNumber(), action.getText());
+    }
+}
