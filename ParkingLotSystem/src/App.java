712c1e191d484bbba5cbfdd08bcd37cd736f6b63
mitalijambhulkar67 <123642577+mitalijambhulkar67@users.noreply.github.com>
2024-04-29 23:17:38 +0530

    Create App.java

---
+++ ParkingLotSystem/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        ParkingLot parkingLot = new ParkingLot(1, 2, 3);
+
+        Vehicle vehicle1 = new Vehicle(VehicleType.TRUCK);
+        parkingLot.parkTheVehicle(vehicle1);
+
+        Vehicle vehicle2 = new Vehicle(VehicleType.TRUCK);
+        parkingLot.parkTheVehicle(vehicle2);
+    }
+}
