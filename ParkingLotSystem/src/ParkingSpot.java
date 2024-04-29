294d181bcb65871525aec01b19e3ab63e618b640
mitalijambhulkar67 <123642577+mitalijambhulkar67@users.noreply.github.com>
2024-04-29 23:14:59 +0530

    Create ParkingSpot.java

---
+++ ParkingLotSystem/src/ParkingSpot.java
@@ -0,0 +1,33 @@
+public class ParkingSpot {
+    private ParkingSpotType parkingSpotType;
+    private boolean isFree;
+    private Vehicle vehicle;
+
+    public ParkingSpot(ParkingSpotType parkingSpotType) {
+        this.parkingSpotType = parkingSpotType;
+    }
+
+    public ParkingSpotType getparkingSpotType() {
+        return this.parkingSpotType;
+    }
+
+    public void setparkingSpotType(ParkingSpotType parkingSpotType) {
+        this.parkingSpotType = parkingSpotType;
+    }
+
+    public boolean getIsFree() {
+        return this.isFree;
+    }
+
+    public void setIsFree(boolean isFree) {
+        this.isFree = isFree;
+    }
+
+    public Vehicle getVehicle() {
+        return this.vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+}
